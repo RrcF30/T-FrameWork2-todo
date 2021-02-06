d0fbcf6060dc8c4c1708a1a7a095c126095d9241
RrcF30 <RrcF30@gmail.com>
2021-02-06 18:45:50 +0900

    error

--- js/app.js
+++ js/app.js
@@ -9,10 +9,14 @@ let month = date.getMonth() + 1;
 let week_number = 0;
 
 // 一週間取得
-let s_week_month = [];
-let s_week_day = [];
-let e_week_month = [];
-let e_week_day = [];
+let s_week_month;
+s_week_month = [];
+let s_week_day;
+s_week_day = [];
+let e_week_month;
+e_week_month = [];
+let e_week_day;
+e_week_day= [];
 let s_criteria = new Date("2021/1/24/12:00");
 let e_criteria = new Date("2021/1/30/12:00");
 let addDate = 7;
